7e0e2fc29970ee06f2fa1b8fdc37790a5408b6d6
NG.D.Trieu <nguyenductrieu.toank30chl@gmail.com>
2025-03-04 22:17:20 +0700

    Init and restructure

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import logo from '../Images/logo.svg';
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>Todo App</div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
