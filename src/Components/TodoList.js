01326ef5195901bdce48bb03123921a06257fe82
NG.D.Trieu <nguyenductrieu.toank30chl@gmail.com>
2025-03-11 11:10:34 +0700

    Change Todolist to function component

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,115 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import Add from "./Add.js"
 import logo from "../Images/logo.svg";
 
-class TodoList extends React.Component {
-    state = {
-        arrActs: [
-            { id: "1", title: "Playing Game", onEdit: false },
-            { id: "2", title: "Reading book", onEdit: false },
-            { id: "3", title: "Playing Guitar", onEdit: false }
-        ],
-        editText: "",
-        editKey: false
+const TodoList = () => {
+    const [arrActs, setArrActs] = useState([
+        { id: "1", title: "Playing Game", onEdit: false },
+        { id: "2", title: "Reading book", onEdit: false },
+        { id: "3", title: "Playing Guitar", onEdit: false }
+    ]);
+
+    const [editText, setEditText] = useState("");
+    const [editKey, setEditKey] = useState(false);
+
+    const handleAdd = (newAct) => {
+        setArrActs([...arrActs, newAct])
     }
 
-    handleAdd = (newAct) => {
-        this.setState({
-            arrActs: [...this.state.arrActs, newAct]
-        })
+    const handleDelete = (act) => {
+        setArrActs(arrActs.filter(item => item.id !== act.id));
     }
 
-    handleDelete = (act) => {
-        this.setState({
-            arrActs: this.state.arrActs.filter(item => item.id !== act.id)
-        })
-    }
 
-    handleEdit = (obj) => {
+    const handleEdit = (obj) => {
         if (this.state.editKey === true) {
             alert("Can't edit 2 act at the same time !");
             return;
         }
-        this.setState(prevState => ({
-            arrActs: prevState.arrActs.map(item =>
+        setArrActs(prevState =>
+            prevState.map(item =>
                 item.id === obj.id
                     ? { ...item, onEdit: !item.onEdit }
                     : item
             )
-        }));
-        this.setState({
-            editKey: true
-        })
+        );
+
+        setEditKey(true);
     }
 
-    handleInput = (event) => {
-        this.setState({
-            editText: event.target.value
-        })
+    const handleInput = (event) => {
+        setEditText(event.target.value);
     }
 
-    handleSaveEdit = (obj) => {
-        this.setState(prevState => ({
-            arrActs: prevState.arrActs.map(item =>
+
+
+    const handleSaveEdit = (obj) => {
+        setArrActs(prevState =>
+            prevState.map(item =>
                 item.id === obj.id
-                    ? { id: item.id, title: this.state.editText, onEdit: false }
+                    ? { id: item.id, title: editText === "" ? item.title : editText, onEdit: false }
                     : item
             )
-        }));
-        this.setState({
-            editText: "",
-            editKey: false
-        })
-    }
+        );
+        setEditText("");
+        setEditKey(false);
+    };
 
-    render() {
-        return (
-            <>
-                <img src={logo} className="App-logo" alt="logo" />
-                <div>Todo App</div>
-                <div className="todoContainer">
-                    <Add
-                        addNewAct={this.handleAdd}
-                    />
 
-                    <div className="List">
-                        {
-                            this.state.arrActs.map((item, index) => {
-                                return (
-                                    <React.Fragment key={item.id}>
-                                        {item.onEdit === false ?
-                                            <div key={item.id}>
-                                                <span>{index + 1} - {item.title}</span> <></>
-                                                <button className="Edit"
-                                                    onClick={() => this.handleEdit(item)}
-                                                >Edit</button> <></>
-                                                <button className="Delete"
-                                                    onClick={() => this.handleDelete(item)}
-                                                >Delete</button>
+    return (
+        <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div>Todo App</div>
+            <div className="todoContainer">
+                <Add
+                    addNewAct={handleAdd}
+                />
+
+                <div className="List">
+                    {
+                        arrActs.map((item, index) => {
+                            return (
+                                <React.Fragment key={item.id}>
+                                    {item.onEdit === false ?
+                                        <div key={item.id}>
+                                            <span>{index + 1} - {item.title}</span> <></>
+                                            <button className="Edit"
+                                                onClick={() => handleEdit(item)}
+                                            >Edit</button> <></>
+                                            <button className="Delete"
+                                                onClick={() => handleDelete(item)}
+                                            >Delete</button>
+                                        </div>
+                                        :
+                                        <>
+                                            <div className="onEdit">
+                                                <span>{index + 1}</span>
+                                                <span> - </span>
+                                                <input type="text" value={editText === "" ? item.title : editText}
+                                                    onChange={(event) => handleInput(event)}
+                                                ></input>
+                                                <button type="button"
+                                                    onClick={() => handleSaveEdit(item)}
+                                                >Save</button>
                                             </div>
-                                            :
-                                            <>
-                                                <div className="onEdit">
-                                                    <span>{index + 1}</span>
-                                                    <span> - </span>
-                                                    <input type="text" value={this.state.editText === "" ? item.title : this.state.editText}
-                                                        onChange={(event) => this.handleInput(event)}
-                                                    ></input>
-                                                    <button type="button"
-                                                        onClick={() => this.handleSaveEdit(item)}
-                                                    >Save</button>
-                                                </div>
-                                            </>
-                                        }
-                                    </React.Fragment>
-                                )
-                            })
-                        }
-                    </div>
+                                        </>
+                                    }
+                                </React.Fragment>
+                            )
+                        })
+                    }
                 </div>
-            </>
-        )
-    }
-}
+            </div>
+        </>
+    )
+};
 
 export default TodoList;
